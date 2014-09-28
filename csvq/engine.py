04edbe8169e9d25563da22838448e26cf9b4ed19
takada <nightly@at-akada.org>
2014-09-28 23:57:49 +0900

    NFA->DFA. spec

--- csvq/engine.py
+++ csvq/engine.py
@@ -13,36 +13,59 @@ class FA(object):
         self.map_ = {}
         self.start = None
         self.accepts = set()
-        self.links = {}
+        self._links = {}
+        self._states = set()
+        self._inputs = set()
+    def __repr__(self):
+        return str(self._states)
     def is_accept_state(self, state):
         return state in self.accepts
-    def get_links(self, state, char=None, exclusive=''):
-        links = self.links.get(state, set())
-        if char is not None:
-            links = {link for link in links if link[1]==char}
+    def get_links(self, state, input_=None, exclusive=''):
+        links = self._links.get(state, set())
+        if input_ is not None:
+            links = {link for link in links if link[0]==input_}
         if exclusive is not None:
-            links = {link for link in links if link[1]!=exclusive}
+            links = {link for link in links if link[0]!=exclusive}
         return links
-    def link(self, state, char, nextstate):
-        link = (state, char)
-        self.links.setdefault(state, set())
-        self.links[state].add(link)
-        self._add_map(link, nextstate)
+    def link(self, state, input_, nextstate):
+        self._inputs.add(input_)
+        self._states.add(state)
+        self._states.add(nextstate)
+        link = (input_, nextstate)
+        self._links.setdefault(state, set())
+        self._links[state].add(link)
+        self._add_map(state, input_, nextstate)
 
 class NFA(FA):
-    def _add_map(self, link, nextstate):
-        self.map_.setdefault(link, set())
-        self.map_[link].add(nextstate)
+    def _add_map(self, state, input_, nextstate):
+        args = (state, input_)
+        self.map_.setdefault(args, set())
+        self.map_[args].add(nextstate)
 
-class DFA(FA): 
-    def _add_map(self, link, nextstate):
-        self.map_[link] = nextstate
+class DFA(FA):
+    def to_table(self):
+        states = list(self._states)
+        inputs = list(self._inputs)
+        states.sort()
+        inputs.sort()
+        table = [[''] + inputs]
+        table += [[state] + [self.map_.get((state, i)) for i in inputs] for state in states]
+        return table
+
+    def _add_map(self, state, input_, nextstate):
+        args = (state, input_)
+        self.map_[args] = nextstate
 
 def convert_nfa2dfa(nfa, factory):
+    newstart, newmap = _convertlink(nfa)
+    return _create_dfa(factory, newmap, nfa, newstart)
+
+def _convertlink(nfa):
     def extends(nfa, states):
         res = set(states)
         for state in states:
-            res.update(nfa.get_links(state, char='', exclusive=None))
+            links = nfa.get_links(state, input_='', exclusive=None)
+            res.update({link[1] for link in links})
 
         return frozenset(res)
 
@@ -52,23 +75,22 @@ def convert_nfa2dfa(nfa, factory):
     newmap = dict()
     while stack:
         curstates = stack.pop()
+        print curstates
         newstates = dict()
         for nfastate in curstates:
-            links = nfa.get_links(nfastate, char=None, exclusive='')
-            for state, char in links:
-                newstates.setdefault(char, set(curstates))
-                newstates[char].remove(nfastate)
-                newstates[char].add(state)
+            links = nfa.get_links(nfastate, input_=None, exclusive='')
+            for input_, state in links:
+                newstates.setdefault(input_, set())
+                newstates[input_].add(state)
 
         statecache.add(frozenset(curstates))
-        for char, states in newstates.items():
+        for input_, states in newstates.items():
             extended = extends(nfa, states)
-            newlink = (curstates, char)
+            newlink = (curstates, input_)
             newmap[newlink] =  extended
             if extended not in statecache:
                 stack.append(extended)
-
-    return _create_dfa(factory, newmap, nfa, newstart)
+    return newstart, newmap
 
 def _create_dfa(factory, map_, nfa, newstart):
     def createnumstate(statecache, factory, state):
@@ -85,14 +107,14 @@ def _create_dfa(factory, map_, nfa, newstart):
     statecache = dict()
     newmap = dict()
     dfa.start = createnumstate(statecache, factory, newstart)
-    for (state0, char), state1 in map_.items():
+    for (state0, input_), state1 in map_.items():
         numstate0 = createnumstate(statecache, factory, state0)
-        if isfinal(state0):
+        if isfinal(state0, nfa):
             dfa.accepts.add(numstate0)
 
         numstate1 = createnumstate(statecache, factory, state1)
-        dfa.link(numstate0, char, numstate1)
-        if isfinal(state1):
+        dfa.link(numstate0, input_, numstate1)
+        if isfinal(state1, nfa):
             dfa.accepts.add(numstate1)
 
     return dfa
--- csvq/test/engine_test.py
+++ csvq/test/engine_test.py
@@ -1,10 +1,19 @@
 from .. import engine
 
-def test_convert_nfa2dfa():
+def test_NFA():
+    u"""
+    test for engine.convert_nfa2dfa
+    """
     fac = engine.StateFactory()
     nfa = engine.NFA()
     states = [fac.new_state() for i in range(10)]
     start = states[0]
+    # sample
+    # 0 -->  1 -a-> 2 -b-> 3 -b-> 4 -d-> 5
+    #   -->  6 -a-> 7 -c-> 8 -c-> 9
+    # dfa:
+    # 0 -a-> 1 -b-> 2 -b-> 3 -d-> 4
+    #          -c-> 5 -c-> 6
     nfa.start = states[0]
     nfa.link(start, '', states[1])
     nfa.link(states[1], 'a', states[2])
@@ -19,6 +28,11 @@ def test_convert_nfa2dfa():
     nfa.link(states[8], 'c', states[9])
     nfa.accepts.add(states[9])
 
+    assert {('', 1), ('',6)} == nfa.get_links(nfa.start, '', exclusive=None)
+    fac = engine.StateFactory()
     dfa = engine.convert_nfa2dfa(nfa, fac)
     assert dfa
-
+    table = dfa.to_table()
+    print table
+    assert 8==len(table)
+    assert ['', 'a', 'b', 'c', 'd'] == table[0]
